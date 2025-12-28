c66320d7e8d3ea247b26fe95ca2b22cd9a61be36
VikMWeb <vik.m.web@gmail.com>
2025-12-28 16:45:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const API_URL = window.API_URL;
+const API_URL = "https://script.google.com/macros/s/AKfycbyP9A9thgbJ7sbakKRpEs9ef4VoqpbGpGOz_Isowr-wXx-ZrPlp_-T-_O6snF-n1jn6/exec";
 
 // якщо вже є сесія — пробуємо відразу зайти
 (async function autoIn(){
