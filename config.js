2b3fd41c62ee25dcd842f66e19698458d4bb3e68
VikMWeb <vik.m.web@gmail.com>
2025-12-28 16:41:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-window.API_URL = "https://script.google.com/macros/s/AKfycbyP9A9thgbJ7sbakKRpEs9ef4VoqpbGpGOz_Isowr-wXx-ZrPlp_-T-_O6snF-n1jn6/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbyP9A9thgbJ7sbakKRpEs9ef4VoqpbGpGOz_Isowr-wXx-ZrPlp_-T-_O6snF-n1jn6/exec";
+
